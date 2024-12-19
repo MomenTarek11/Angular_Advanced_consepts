3dab990c4a37fe0e29af9d0b4173c118977f4b62
MomenTarek11 <116780849+MomenTarek11@users.noreply.github.com>
2024-12-19 23:44:42 +0200

    get the api to test

---
+++ Test_Consepts/src/app/services/users.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private http: HttpClient) {}
+  getUsers() {
+    return this.http.get(`${environment.apiUrl}`);
+  }
+}
--- Test_Consepts/src/app/signals/writable-signal/writable-signal.component.ts
+++ Test_Consepts/src/app/signals/writable-signal/writable-signal.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-writable-signal',
   standalone: true,
   imports: [],
   templateUrl: './writable-signal.component.html',
-  styleUrl: './writable-signal.component.scss'
+  styleUrl: './writable-signal.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WritableSignalComponent {
-
+export class WritableSignalComponent implements OnInit {
+  users: any[] = [];
+  constructor(private usersService: UsersService) {}
+  ngOnInit(): void {
+    this.usersService.getUsers().subscribe((users) => {
+      console.log(users);
+    });
+  }
 }
