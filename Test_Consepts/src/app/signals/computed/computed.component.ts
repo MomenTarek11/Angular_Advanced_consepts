74eafaf8c6005aadbe730043ea448b017a52c461
MomenTarek11 <116780849+MomenTarek11@users.noreply.github.com>
2024-12-29 14:37:53 +0200

    Set up the angular material

--- Test_Consepts/src/app/signals/computed/computed.component.ts
+++ Test_Consepts/src/app/signals/computed/computed.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { UsersService } from '../../services/users.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-computed',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './computed.component.html',
-  styleUrl: './computed.component.scss'
+  styleUrl: './computed.component.scss',
 })
 export class ComputedComponent {
-  
+  value: string = '';
+  users: any[] = [];
+  constructor(
+    private usersService: UsersService,
+    private spinner: NgxSpinnerService
+  ) {}
+  ngOnInit(): void {
+    this.spinner.show();
+    this.usersService.getUsers().subscribe((users: any) => {
+      this.spinner.hide();
+      this.users = users;
+    });
+  }
 }
