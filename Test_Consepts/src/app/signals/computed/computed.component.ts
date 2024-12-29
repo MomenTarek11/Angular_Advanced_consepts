088a1bed6a54fb6a01abef39a3ac2018144a5fb8
MomenTarek11 <116780849+MomenTarek11@users.noreply.github.com>
2024-12-29 15:02:04 +0200

    Make search with computed signal

--- Test_Consepts/src/app/signals/computed/computed.component.ts
+++ Test_Consepts/src/app/signals/computed/computed.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Signal } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-computed',
   standalone: true,
@@ -15,13 +16,23 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     FormsModule,
     MatButtonModule,
     MatIconModule,
+    CommonModule,
   ],
   templateUrl: './computed.component.html',
   styleUrl: './computed.component.scss',
 })
 export class ComputedComponent {
-  value: string = '';
-  users: any[] = [];
+  searchText = signal<string>('');
+  users = signal<any[]>([]);
+  computedUsers = computed(() => {
+    if (this.searchText()) {
+      return this.users().filter((user) =>
+        user.username.toLowerCase().includes(this.searchText().toLowerCase())
+      );
+    } else {
+      return this.users();
+    }
+  });
   constructor(
     private usersService: UsersService,
     private spinner: NgxSpinnerService
@@ -29,8 +40,11 @@ export class ComputedComponent {
   ngOnInit(): void {
     this.spinner.show();
     this.usersService.getUsers().subscribe((users: any) => {
+      this.users.set(users);
       this.spinner.hide();
-      this.users = users;
     });
+  }
+  Search(searchText: string) {
+    this.searchText.set(searchText);
   }
 }
